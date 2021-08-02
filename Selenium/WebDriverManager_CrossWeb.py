7d9cd1ec5357471172c130b4ce22cef6172fe93a
Arifuzzaman Munaf <arifuzzamanmunaf@gmail.com>
2021-08-02 17:53:28 +0600

    Updated Code

--- Selenium/WebDriverManager_CrossWeb.py
+++ Selenium/WebDriverManager_CrossWeb.py
@@ -5,3 +5,4 @@ from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
 
 driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+driver.get('http://www.youtube.com')
