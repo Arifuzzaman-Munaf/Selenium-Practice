02209a1d73a2b399516ff88d112950e10dcfa2d3
Arifuzzaman Munaf <arifuzzamanmunaf@gmail.com>
2021-08-02 15:22:40 +0600

    added codes
    added codes

---
+++ Selenium/WebDriverManager_CrossWeb.py
@@ -0,0 +1,7 @@
+
+from selenium.webdriver.common.by import By
+import time
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+
+driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
---
+++ Selenium/webDriver.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+driver = webdriver.Firefox(executable_path = "F:\\Practice Codes\\geckodriver.exe")
+driver.implicitly_wait(1)
+driver.get("http://www.google.com")
+print(driver.title)
+
+driver.find_element(By.NAME, 'q').send_keys('Django')
+options = driver.find_elements(By.CSS_SELECTOR, 'ul.erkvQe li div span')
+
+for i in options:
+    print(i.text)
+    if i.text == "django channels" :
+        i.click()
+        break
+
+time.sleep(3)
+driver.quit()
