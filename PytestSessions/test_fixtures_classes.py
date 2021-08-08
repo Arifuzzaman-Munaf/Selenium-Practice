c245d9239d346be4d90844448a1f494da49762fc
Arifuzzaman Munaf <arifuzzamanmunaf@gmail.com>
2021-08-08 08:10:57 +0600

    Python fixtures in details with report

---
+++ PytestSessions/test_fixtures_classes.py
@@ -0,0 +1,28 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
+
+@pytest.fixture(params=['edge', 'firefox'],scope='class')
+def init_driver(request):
+    if request.param == 'edge':
+        web_driver = webdriver.Edge(EdgeChromiumDriverManager().install())
+
+    if request.param == 'firefox' :
+        web_driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+
+    request.cls.driver = web_driver
+
+    yield
+    web_driver.close()
+
+@pytest.mark.usefixtures('init_driver')
+class BaseTest :
+    pass
+
+class Test_google(BaseTest):
+
+    def test_google_title(self):
+        self.driver.get('http://www.google.com')
+        assert self.driver.title == "Google"
